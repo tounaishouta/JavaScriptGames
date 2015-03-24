79298e3ddaaeeab5486a1b0dfc1d6d6adbd0aee6
TOUNAI Shouta <tounai.shouta@gmail.com>
2015-03-24 18:39:18 +0900

    dodge: touch up
    modified:   dodge.js

--- dodge.js
+++ dodge.js
@@ -5,26 +5,23 @@
 
   // example
   /*
-  function g() {
-    if ((frame + FPS) % (FPS * 5) === 0) {
-      reds.push(new Ball('#f00', -1, FPS, 0, 0));
-    }
-    return true;
-  }
+  var generateArray = [];
+  for (var i = 0; i < 100; i++)
+    generateArray.push({ x: 0.5, y: 0.5 });
 
-  function c() {
-    blue.dx += accelRate * Math.cos(frame / 17);
-    blue.dy += accelRate * Math.sin(frame / 19);
-    return true;
-  }
+  var controlArray = [];
+  for (var i = 0; i < 1000; i++)
+    controlArray.push({ x: Math.cos(i / 6), y: -Math.cos(i / 6) });
 
   setTimeout(function () {
-    console.log(toSecond(evaluate(g, c)));
-    replay(g, c);
+    var generate = generateFromArray(generateArray);
+    var control  = controlFromArray(controlArray);
+    console.log(toSecond(evaluate(generate, control)));
+    replay(generate, control);
   });
   */
 
-  var version = '8.9';
+  var version = '9.0';
 
   var FPS = 25;
   var radius = 0.01;
@@ -34,93 +31,7 @@
   var accelRate = 0.004;
   var touchRatio = 10;
 
-  function Ball(color, charge, wait, x, y) {
-    this.color  = color;
-    this.charge = charge;
-    this.wait   = wait;
-    this.x      = x;
-    this.y      = y;
-    this.dx     = 0;
-    this.dy     = 0;
-  }
-
-  function updateBall(ball) {
-    if (ball.wait > 0) {
-      ball.wait--;
-      return;
-    }
-    var v = Math.sqrt(ball.dx * ball.dx + ball.dy * ball.dy);
-    ball.dx *= Math.exp(friction * v);
-    ball.dy *= Math.exp(friction * v);
-    ball.x  += ball.dx;
-    ball.y  += ball.dy;
-    ball.x  -= Math.round(ball.x);
-    ball.y  -= Math.round(ball.y);
-  }
-
-  function coulomb(a, b) {
-    if (a.wait > 0 || b.wait > 0)
-      return;
-    var x = 2 * Math.PI * (a.x - b.x);
-    var y = 2 * Math.PI * (a.y - b.y);
-    var r = Math.sqrt(2 - Math.cos(x) - Math.cos(y));
-    if (r > 0) {
-      a.dx += coulombConst * a.charge * b.charge * Math.sin(x) / r / (r * r + epsilon);
-      a.dy += coulombConst * a.charge * b.charge * Math.sin(y) / r / (r * r + epsilon);
-    }
-  }
-
-  function isCaught(a, b) {
-    if (a.wait > 0 || b.wait > 0)
-      return false;
-    var D = (2 * radius) * (2 * radius);
-    var dx = a.dx - b.dx;
-    var dy = a.dy - b.dy;
-    var A = dx * dx + dy * dy;
-    for (var i = -1; i <= +1; i++) {
-      for (var j = -1; j <= +1; j++) {
-        var x = a.x - b.x + i;
-        var y = a.y - b.y + j;
-        var B = dx * x + dy * y;
-        var C = x * x + y * y;
-        if (B <= 0 && C <= D)
-          return true;
-        if (0 < B && B < A && C - B * B / A <= D) {
-          var t = B / A;
-          a.x -= t * a.dx;
-          a.y -= t * a.dy;
-          b.x -= t * b.dx;
-          b.y -= t * b.dy;
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-
   var frame, blue, reds, mode, time;
-
-  function initialize() {
-    frame = 0;
-    blue = new Ball('#00f', +1, 0, 0, 0);
-    reds = [];
-    reds.push(new Ball('#f00', -1, 0, -0.3, -0.3));
-    reds.push(new Ball('#f00', -1, 0, +0.3, +0.3));
-  }
-
-  function update() {
-    frame++;
-    reds.forEach(function (red) {
-      coulomb(red, blue);
-      reds.forEach(function (red2) { coulomb(red, red2); });
-    });
-    updateBall(blue);
-    reds.forEach(updateBall);
-  }
-
-  function isGameOver() {
-    return reds.some(function (red) { return isCaught(blue, red); });
-  }
 
   function evaluate(generate, control) {
     initialize();
@@ -158,6 +69,35 @@
         return;
       }
     }, 1000 / FPS);
+  }
+
+  function generateFromArray(array) {
+    return function () {
+      if ((frame + FPS) % (5 * FPS) === 0) {
+        var coord = array[(frame + FPS) / (5 * FPS)];
+        if (coord)
+          reds.push(new Ball('#f00', -1, FPS, coord.x, coord.y));
+        else
+          return false;
+      }
+      return true;
+    };
+  }
+
+  function controlFromArray(array) {
+    return function () {
+      var accel = array[frame];
+      if (!accel)
+        return false;
+      var r = Math.sqrt(accel.x * accel.x + accel.y * accel.y);
+      if (r > 1) {
+        accel.x /= r;
+        accel.y /= r;
+      }
+      blue.dx += accelRate * accel.x;
+      blue.dy += accelRate * accel.y;
+      return true;
+    };
   }
 
   function newGame() {
@@ -275,14 +215,100 @@
       ontap = f;
   }
 
+  function initialize() {
+    frame = 0;
+    blue = new Ball('#00f', +1, 0, 0, 0);
+    reds = [];
+    reds.push(new Ball('#f00', -1, 0, -0.3, -0.3));
+    reds.push(new Ball('#f00', -1, 0, +0.3, +0.3));
+  }
+
+  function update() {
+    frame++;
+    reds.forEach(function (red) {
+      coulomb(red, blue);
+      reds.forEach(function (red2) { coulomb(red, red2); });
+    });
+    updateBall(blue);
+    reds.forEach(updateBall);
+  }
+
+  function isGameOver() {
+    return reds.some(function (red) { return isCaught(blue, red); });
+  }
+
+  function Ball(color, charge, wait, x, y) {
+    this.color  = color;
+    this.charge = charge;
+    this.wait   = wait;
+    this.x      = x;
+    this.y      = y;
+    this.dx     = 0;
+    this.dy     = 0;
+  }
+
+  function updateBall(ball) {
+    if (ball.wait > 0) {
+      ball.wait--;
+      return;
+    }
+    var v = Math.sqrt(ball.dx * ball.dx + ball.dy * ball.dy);
+    ball.dx *= Math.exp(friction * v);
+    ball.dy *= Math.exp(friction * v);
+    ball.x  += ball.dx;
+    ball.y  += ball.dy;
+    ball.x  -= Math.round(ball.x);
+    ball.y  -= Math.round(ball.y);
+  }
+
+  function coulomb(a, b) {
+    if (a.wait > 0 || b.wait > 0)
+      return;
+    var x = 2 * Math.PI * (a.x - b.x);
+    var y = 2 * Math.PI * (a.y - b.y);
+    var r = Math.sqrt(2 - Math.cos(x) - Math.cos(y));
+    if (r > 0) {
+      a.dx += coulombConst * a.charge * b.charge * Math.sin(x) / r / (r * r + epsilon);
+      a.dy += coulombConst * a.charge * b.charge * Math.sin(y) / r / (r * r + epsilon);
+    }
+  }
+
+  function isCaught(a, b) {
+    if (a.wait > 0 || b.wait > 0)
+      return false;
+    var D = (2 * radius) * (2 * radius);
+    var dx = a.dx - b.dx;
+    var dy = a.dy - b.dy;
+    var A = dx * dx + dy * dy;
+    for (var i = -1; i <= +1; i++) {
+      for (var j = -1; j <= +1; j++) {
+        var x = a.x - b.x + i;
+        var y = a.y - b.y + j;
+        var B = dx * x + dy * y;
+        var C = x * x + y * y;
+        if (B <= 0 && C <= D)
+          return true;
+        if (0 < B && B < A && C - B * B / A <= D) {
+          var t = B / A;
+          a.x -= t * a.dx;
+          a.y -= t * a.dy;
+          b.x -= t * b.dx;
+          b.y -= t * b.dy;
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
   addEventListener('resize', draw);
 
-  var canvas = document.getElementById('canvas');
+  var canvas  = document.getElementById('canvas');
   var context = canvas.getContext('2d');
 
   function draw() {
     canvas.height = canvas.clientHeight;
-    canvas.width = canvas.clientWidth;
+    canvas.width  = canvas.clientWidth;
 
     var scale = Math.floor(Math.min(canvas.height, canvas.width) * 5 / 7);
     context.setTransform(scale, 0, 0, scale, canvas.width / 2, canvas.height / 2);
@@ -291,7 +317,7 @@
     context.fillRect(-1.5, -1.5, 3, 3);
 
     context.strokeStyle = '#0a0';
-    context.lineWidth = 0.002;
+    context.lineWidth   = 0.002;
     context.strokeRect(-1.5, -0.5, 3, 1);
     context.strokeRect(-0.5, -1.5, 1, 3);
 
@@ -301,17 +327,17 @@
     context.setTransform(1, 0, 0, 1, 0, 0);
     context.fillStyle = '#000';
 
-    context.font = String(Math.floor(scale / 10)) + 'px Courier, monospace';
-    context.textAlign = 'left';
+    context.font         = String(Math.floor(scale / 10)) + 'px Courier, monospace';
+    context.textAlign    = 'left';
     context.textBaseline = 'top';
     context.fillText(toSecond(frame), 0, 0);
-    context.font = String(Math.floor(scale / 20)) + 'px Courier, monospace';
-    context.textAlign = 'right';
+    context.font         = String(Math.floor(scale / 20)) + 'px Courier, monospace';
+    context.textAlign    = 'right';
     context.textBaseline = 'bottom';
     context.fillText('ver ' + version, canvas.width, canvas.height);
 
-    context.font = String(Math.floor(scale / 10)) + 'px Courier, monospace';
-    context.textAlign = 'center';
+    context.font         = String(Math.floor(scale / 10)) + 'px Courier, monospace';
+    context.textAlign    = 'center';
     context.textBaseline = 'middle';
     switch (mode) {
       case 'newGame':
@@ -323,10 +349,10 @@
       case 'ranking':
         var x0 = (canvas.width - scale) / 2;
         var y0 = (canvas.height - scale) / 2 + scale / 10 / 2;
-        context.font = String(Math.floor(0.08 * scale)) + 'px Courier, monospace';
+        context.font         = String(Math.floor(0.08 * scale)) + 'px Courier, monospace';
         context.textBaseline = 'middle';
-        context.textAlign = 'center';
-        context.fillStyle = '#000';
+        context.textAlign    = 'center';
+        context.fillStyle    = '#000';
         context.fillText('Ranking', x0 + scale / 2, y0);
         for (var i = 1; i <= 9 && rankData[i] !== false; i++) {
           context.fillStyle = i == rankOrder ? '#f00' : '#000';
@@ -372,6 +398,25 @@
       setTimeout(ontap);
   }
 
+  var touchState = {};
+
+  function touchstart(id, x, y) {
+    touchState[id] = { x0: x, y0: y, x: x, y: y };
+    setTimeout(ontap);
+  }
+
+  function touchmove(id, x, y) {
+    if (touchState[id]) {
+      touchState[id].x = x;
+      touchState[id].y = y;
+    }
+  }
+
+  function touchend(id) {
+    if (touchState[id])
+      delete touchState[id];
+  }
+
   addEventListener('mousedown', mouseEventHandler);
   addEventListener('mousemove', mouseEventHandler);
   addEventListener('mouseup', mouseEventHandler);
@@ -412,24 +457,5 @@
           break;
       }
     }
-  }
-
-  var touchState = {};
-
-  function touchstart(id, x, y) {
-    touchState[id] = { x0: x, y0: y, x: x, y: y };
-    setTimeout(ontap);
-  }
-
-  function touchmove(id, x, y) {
-    if (touchState[id]) {
-      touchState[id].x = x;
-      touchState[id].y = y;
-    }
-  }
-
-  function touchend(id) {
-    if (touchState[id])
-      delete touchState[id];
   }
 })();
