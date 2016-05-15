0c46cb7814a4f7dc8ce5a9fa891b233cc8bdbfe7
TOUNAI Shouta <tounai.shouta@gmail.com>
2016-05-15 17:02:54 +0900

    modified

--- sunflower.js
+++ sunflower.js
@@ -1,7 +1,7 @@
 (function() {
   "strict";
 
-  let timeoutID = -1;
+  let timeoutID = null;
 
   const size   = 640;
   const canvas  = document.getElementById("canvas");
@@ -11,8 +11,9 @@
 
     event.preventDefault();
 
-    if (timeoutID != -1) {
+    if (timeoutID !== null) {
       clearTimeout(timeoutID);
+      timeoutID = null;
     }
 
     const dtheta = 2 * Math.PI
@@ -20,6 +21,7 @@
       / Number(document.getElementById("denom").value);
     const radius = 1 / Number(document.getElementById("radius").value);
     const speed  = Number(document.getElementById("speed").value);
+    const edge   = document.getElementById("edge").checked;
 
     canvas.width  = size;
     canvas.height = size;
@@ -30,60 +32,71 @@
     context.arc(0, 0, 1, 0, 2 * Math.PI);
     context.fill();
 
-    const seeds = [];
-
-    const newSeed = function(r, theta) {
-
+    const drawSeed = function(s) {
       context.lineWidth   = radius / 16;
       context.fillStyle   = "yellow";
       context.strokeStyle = "orange";
-
       context.beginPath();
       context.arc(
-          r * Math.cos(theta),
-          r * Math.sin(theta),
+          s.r * Math.cos(s.theta),
+          s.r * Math.sin(s.theta),
           radius,
           0,
           2 * Math.PI);
       context.closePath();
       context.fill();
       context.stroke();
-
-      seeds.push({ r: r, theta: theta });
     };
 
-    const rNext = function(theta) {
-      let r = 0;
-      seeds.forEach(function(seed) {
-        const phi = seed.theta - theta;
-        const x   = seed.r * Math.cos(phi);
-        const y   = seed.r * Math.sin(phi);
-        const R   = 2 * radius;
-        if (Math.abs(y) < R) {
-          r = Math.max(r, x + Math.sqrt(R * R - y * y));
-        }
-      });
-      return r;
+    const drawEdge = function(s0, s1) {
+      context.lineWidth   = radius / 8;
+      context.strokeStyle = "blue";
+      context.beginPath();
+      context.moveTo(s0.r * Math.cos(s0.theta), s0.r * Math.sin(s0.theta));
+      context.lineTo(s1.r * Math.cos(s1.theta), s1.r * Math.sin(s1.theta));
+      context.stroke();
     };
+
+    const seeds = [];
 
     let theta = 0;
-    let count = 0;
 
     const loop = function() {
 
-      const r = rNext(theta);
+      let r = 0;
+      let i0 = null;
+      for (let i = 0; i < seeds.length; i++) {
+        const phi = seeds[i].theta - theta;
+        const x   = seeds[i].r * Math.cos(phi);
+        const y   = seeds[i].r * Math.sin(phi);
+        const R   = 2 * radius;
+        const dx2 = R * R - y * y;
+        if (dx2 > 0) {
+          const X = x + Math.sqrt(dx2);
+          if (X > r) {
+            r = X;
+            i0 = i;
+          }
+        }
+      }
+
       if (r + radius > 1) {
-        alert(count + " seeds");
+        alert(seeds.length + " seeds are packed.");
         return;
       }
-      newSeed(r, theta);
+
+      const seed = { r: r, theta: theta }
+      drawSeed(seed);
+      if (edge && i0 !== null) {
+        drawEdge(seed, seeds[i0])
+      }
+      seeds.push(seed);
 
       theta += dtheta;
       theta %= 2 * Math.PI;
-      count += 1;
 
       timeoutID = setTimeout(loop, speed);
-    }
+    };
 
     timeoutID = setTimeout(loop, 0);
   };
